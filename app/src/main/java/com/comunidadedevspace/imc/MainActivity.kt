aefef39642e2121883fadea2a59d961960fd574d
EGEMERASCA <dudugemerasca@gmail.com>
2024-11-25 21:46:43 +0000

    colorindo app + icones

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.comunidadedevspace.imc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
@@ -20,8 +21,8 @@ class MainActivity : AppCompatActivity() {
             val pesoStr: String = edtPeso.text.toString()
             val alturaStr: String = edtAltura.text.toString()
 
-            if(pesoStr == "" || alturaStr ==""){
-                //mostrar msg pro usuario
+            if (pesoStr == "" || alturaStr == "") {
+
                 Snackbar
                     .make(
                         edtPeso,
@@ -38,8 +39,15 @@ class MainActivity : AppCompatActivity() {
                 val alturaq2 = altura * altura
                 val resultado = peso / alturaq2
 
-                println("Seu imc é: " + resultado)
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+                //cores
+                // main edict txt background + icne
+                //gradiente icone descriçao
             }
         }
     }
 }
+
