86c705f98448713f621e766914832b4ddd8ad1b5
EGEMERASCA <dudugemerasca@gmail.com>
2024-11-26 00:50:37 +0000

    ..

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -42,10 +42,6 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this, ResultActivity::class.java)
                 intent.putExtra(KEY_RESULT_IMC, resultado)
                 startActivity(intent)
-
-                //cores
-                // main edict txt background + icne
-                //gradiente icone descriçao
             }
         }
     }
